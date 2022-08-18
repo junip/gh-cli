79c2655167f7bbef9c3dee1d6e054681f913f43c
Junip Dewan <junipd2@gmail.com>
2022-08-18 21:46:39 +0530

    .env changes

--- connect/apiClient.go
+++ connect/apiClient.go
@@ -2,18 +2,27 @@ package connect
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
+
+	"github.com/joho/godotenv"
 )
 
 func ApiClient() *github.Client {
-	value := os.Getenv("token")
 	context := context.Background()
+	envLoadErr := godotenv.Load()
+
+	if envLoadErr != nil {
+		fmt.Print("please create .env file in your root folder")
+	}
+
+	apiToken := os.Getenv("API_TOKEN")
 
 	tokenService := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: value},
+		&oauth2.Token{AccessToken: apiToken},
 	)
 
 	tokenClient := oauth2.NewClient(context, tokenService)
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	google.golang.org/appengine v1.6.7 // indirect
